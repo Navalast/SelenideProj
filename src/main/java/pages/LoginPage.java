9b7033790907358f54d6da33a9055f04f97cd5ba
Navalast <thegotothego@mail.ru>
2024-08-12 17:29:04 +0300

    refactoring

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -8,14 +8,27 @@ import static com.codeborne.selenide.Selenide.page;
 
 @Getter
 public class LoginPage {
+
     public LoginPage() {
         page(this);
     }
 
-    public LoginPage auth(String login, String password) {
+    private void enterLogin(String login) {
         $(By.id("user-name")).sendKeys(login);
+    }
+
+    private void enterPassword(String password) {
         $(By.id("password")).sendKeys(password);
+    }
+
+    private void clickLoginButton() {
         $(By.id("login-button")).click();
+    }
+
+    public LoginPage auth(String login, String password) {
+        enterLogin(login);
+        enterPassword(password);
+        clickLoginButton();
         return page(LoginPage.class);
     }
 }
