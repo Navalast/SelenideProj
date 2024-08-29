d24f0d298d8dde2f0d67b866b56f488a71a17f77
Navalast <thegotothego@mail.ru>
2024-08-29 16:52:29 +0300

    allure

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -5,7 +5,7 @@ pipeline {
         stage('Build and Test') {
             steps {
                 script {
-                    docker.image('my-jenkins').inside {
+                    docker.image('my-jenkins-container').inside {
                         sh 'mvn clean install'
                     }
                 }
