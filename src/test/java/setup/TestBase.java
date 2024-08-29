d028b2b3f63b5df646a96cf4faa9eb12ed132663
Navalast <thegotothego@mail.ru>
2024-08-29 15:37:39 +0300

    allure

--- src/test/java/setup/TestBase.java
+++ src/test/java/setup/TestBase.java
@@ -2,12 +2,12 @@ package setup;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideDriver;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.Step;
 import io.qameta.allure.listener.TestLifecycleListener;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -17,10 +17,14 @@ public abstract class TestBase implements TestLifecycleListener {
     @Step("Настройка окружения")
     @BeforeTest
     public void setup() {
-        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver");
+        WebDriverManager.chromedriver().setup();
+        Configuration.browser = "chrome";
+//        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver");
         Configuration.browserSize = "1920x1080";
         Configuration.headless = true;
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        Configuration.baseUrl = "https://www.saucedemo.com/";
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");  // Запускает браузер в защищенном режиме
